7b910383140f8d1bb5c7c016a2e03705ff4befee
nickcarva <carvalhonick2002@gmail.com>
2020-08-22 18:30:00 -0300

    Pagination implemented at Contact list view

--- contatos/views.py
+++ contatos/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Contato
 
 
 def index(request):
     contatos = Contato.objects.all()
+    paginator = Paginator(contatos, 3)
+
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+
     return render(request, 'contatos/index.html', {
         'contatos': contatos
     })
