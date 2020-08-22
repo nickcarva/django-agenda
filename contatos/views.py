bbd1dd875a164e834b8198f3803c383d103b789d
nickcarva <carvalhonick2002@gmail.com>
2020-08-22 19:39:30 -0300

    Contacts order by id

--- contatos/views.py
+++ contatos/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
+from django.http import Http404
+
 from .models import Contato
 
 
 def index(request):
-    contatos = Contato.objects.all()
+    contatos = Contato.objects.order_by('-id').filter(mostrar=True)
     paginator = Paginator(contatos, 3)
 
     page = request.GET.get('p')
@@ -16,8 +18,11 @@ def index(request):
 
 
 def ver_contato(request, contato_id):
-    # contato = Contato.objects.get(id=contato_id)
     contato = get_object_or_404(Contato, id=contato_id)
+
+    if not contato.mostrar:
+        raise Http404()
+
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato
     })
