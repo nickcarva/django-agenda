3933559e947e6b074e43f524052d97ef217e7a1c
nickcarva <carvalhonick2002@gmail.com>
2020-08-22 18:08:44 -0300

    'mostrar' field added at the contact

--- contatos/admin.py
+++ contatos/admin.py
@@ -4,11 +4,12 @@ from .models import Categoria, Contato
 
 class ContetoAdmin(admin.ModelAdmin):
     list_display = ('id', 'nome', 'sobrenome', 'telefone',
-                    'email', 'data_criacao', 'categoria')
+                    'email', 'data_criacao', 'categoria', 'mostrar')
     list_display_links = ('id', 'nome', 'sobrenome')
     list_filter = ('nome', 'sobrenome')
     list_per_page = 10
     search_fields = ('nome', 'sobrenome', 'telefone', 'email')
+    list_editable = ('telefone', 'mostrar')
 
 
 admin.site.register(Categoria)
