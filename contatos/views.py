c078dd37fde583c2a3573781847a6dd191f34b0a
nickcarva <carvalhonick2002@gmail.com>
2020-08-23 08:35:01 -0300

    Search field implemented

--- contatos/views.py
+++ contatos/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from django.http import Http404
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
 
 from .models import Contato
 
@@ -25,4 +27,24 @@ def ver_contato(request, contato_id):
 
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato
+    })
+
+
+def busca(request):
+    termo = request.GET.get('termo')
+    if termo is None or not termo:
+        raise Http404()
+
+    campos = Concat('nome', Value(' '), 'sobrenome')
+
+    contatos = Contato.objects.annotate(nome_completo=campos).filter(
+        Q(nome_completo__icontains=termo) | Q(telefone__icontains=termo)
+    )
+    paginator = Paginator(contatos, 3)
+
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+
+    return render(request, 'contatos/busca.html', {
+        'contatos': contatos
     })
