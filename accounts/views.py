c77bad183504c3dc488e18370444c89b11ceb68f
nickcarva <carvalhonick2002@gmail.com>
2020-08-24 11:13:52 -0300

    Getting the accounts app prepared

---
+++ accounts/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+# from django.contrib import messages
+
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    return render(request, 'accounts/logout.html')
+
+
+def cadastro(request):
+    print(request.POST)
+    return render(request, 'accounts/cadastro.html')
+
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
